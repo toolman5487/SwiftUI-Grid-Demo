7c26f6e3193e34366aa488acc8a155214f4b9684
Willy <willy548798@gmail.com>
2024-12-18 16:21:52 +0800

    Initial Commit

---
+++ HelloGrid/HelloGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloGridApp.swift
+//  HelloGrid
+//
+//  Created by Willy Hsu on 2024/12/18.
+//
+
+import SwiftUI
+
+@main
+struct HelloGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
